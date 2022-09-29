0c45575b455b01695e583279e1e345905f823b13
Akbarshokh <akbarshokh2001@gmail.com>
2022-09-29 09:49:17 +0500

    style: formated the code

--- pages/tests.py
+++ pages/tests.py
@@ -4,31 +4,31 @@ from .views import HomePageView
 
 
 class HomePageTests(SimpleTestCase):
-    
     def setUp(self):
-        url = reverse('home')
+        url = reverse("home")
         self.response = self.client.get(url)
 
     def test_homepage_status_code(self):
-        response = self.client.get('/')
+        response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
-         
+
     def test_homepage_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
-    
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")
+
     def test_homepage_contains_correct_html(self):
-        response = self.client.get('/')
-        self.assertContains(response, 'Homepage')
+        response = self.client.get("/")
+        self.assertContains(response, "Homepage")
 
     def test_homepage_does_not_contain_incorrect_html(self):
-        response = self.client.get('/')
+        response = self.client.get("/")
         self.assertNotContains(
-            response, 'Hi there! I should not be on the page.')
-        
+            response, "Hi there! I should not be on the page."
+        )
+
     def test_homepage_url_resolves_homepageview(self):
-        view = resolve('/')
+        view = resolve("/")
         self.assertEqual(
-            view.func.__name__,
+            view.func.__name__, 
             HomePageView.as_view().__name__
         )
